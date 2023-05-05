06f59fd7b85096ae10999513f5268f31aca4a6b2
GregV3 <grzegorz.muszynski77@gmail.com>
2023-05-05 07:37:56 +0200

    CSS improvements

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,7 +6,7 @@ export default function About() {
     return (
         <div className='About'>
             <h1>Nasza misja</h1>
-            <p>Zespół Gabinetów Lekarskich jest polską, innowacyjną, dynamicznie rozwijająca się placówką, która istnieje na rynku medycznym od 1999 roku.
+            <p>Zespół Gabinetów Lekarskich Medicl-Vita jest polską, innowacyjną, dynamicznie rozwijająca się placówką, która istnieje na rynku medycznym od 1999 roku.
             Świadczymy niepubliczne usługi zdrowotne. W naszym zespole macie Państwo dostęp do lekarzy o wynthsokich specjalnościach medycznych, cenionych i oddanych pacjentowi. Ich doświadczenie, wieloletnia praktyka, a przede wszystkim wiedza pozwolą Państwu na możliwość powrotu do zdrowia.</p>
 
             <p>Nasz zespół oferuje usługi medyczne na wysokim poziomie i miłą obsługę. Rejestracja odbywa się osobiście albo telefonicznie.</p>
